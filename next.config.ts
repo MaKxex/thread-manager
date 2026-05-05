d7e50b14894e00874871afce07f2c4fa07822441
MaKxex <maksimos651@gmail.com>
2026-05-06 02:49:23 +0300

    turbo fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbo: {
+    resolveAlias: {
+      "@/generated/prisma/client": path.join(__dirname, "src/generated/prisma/client"),
+    },
+  },
 };
 
 export default nextConfig;
